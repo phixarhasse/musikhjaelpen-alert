0902acf5672dd7c76cbcf9892437a756f25e1aff
Joel Andersson <joeland@student.chalmers.se>
2024-11-20 22:38:30 +0100

    Update message format, implement proper logging

--- notification-server/scraper.py
+++ notification-server/scraper.py
@@ -5,92 +5,106 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
+import websockets.connection
+import websockets
+import logging
+import asyncio
 import time
 import os
-import asyncio
-import websockets
-import websockets.connection
 
 
-# TODO: Error handling, improved logging
-# TODO: Add env vars for WS URL and page refresh interval
 # TODO: Philips Hue integration
+# TODO: Rename program
+# TODO: Startup script
 
 async def main():
-    print("Starting donation monitor...")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="{asctime} - {levelname} - {message}",
+        style="{", datefmt="%Y-%m-%d %H:%M"
+        )
+    logging.info("Starting donation monitor...")
 
     # Load environment variables
     load_dotenv()
-    previous_value = int(os.environ.get("START_VALUE") or "0")
-    url = os.environ.get("MH_URL")
-    chrome_driver_path = os.environ.get("CHROME_DRIVER_PATH")
+    START_VALUE = int(os.environ.get("START_VALUE") or "0")
+    MH_URL = os.environ.get("MH_URL")
+    CHROMEDRIVER_PATH = os.environ.get("CHROME_DRIVER_PATH")
+    WS_URL = os.environ.get("WEBSOCKET_SERVER_URL")
+    REFRESH_RATE = int(os.environ.get("REFRESH_RATE") or "5") # seconds
 
-    ws_connection = await websockets.connect(uri="ws://localhost:8765", ping_timeout=None)
-    print("Connected to WS server")
+    try:
+        ws_connection = await websockets.connect(uri=WS_URL, ping_timeout=None)
+        logging.info("Connected to WS server")
+    except Exception as e:
+        logging.error(f"Error connecting to WS server: {e}")
+        exit(1)
 
-    # Setup Selenium
+    # Setup Selenium Scraper
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    service = Service(chrome_driver_path)
+    service = Service(CHROMEDRIVER_PATH)
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.get(url)
+    driver.get(MH_URL)
     time.sleep(5)  # Give page time to load
-    print("Page loaded, scraper started.")
+    logging.info("Page loaded, scraper started.")
 
+    previous_value: int = START_VALUE
     try:
         while True:
             try:
                 # FOR TESTING
-                # previous_value = 0
+                #previous_value = 0
 
                 driver.refresh()
                 # Wait for the spinner element to disappear, i.e. the raised amount has been loaded
                 WebDriverWait(driver, 30).until_not(
                     EC.presence_of_element_located(
-                        (By.CLASS_NAME, "entry-amount-module--spinnerWrapper--70e75"))
-                )
+                        (By.CLASS_NAME, "entry-amount-module--spinnerWrapper--70e75")))
 
+                # Find charity total element  
                 charity_total_element = driver.find_element(
                     By.CLASS_NAME, "entry-amount-module--amount--5ecff")
 
                 if charity_total_element.text:
-                    print(
-                        f"Current charity total: {charity_total_element.text}")
-                    total_text = charity_total_element.text[:-2].replace(
-                        ' ', '')
+                    logging.info(f"Current charity total: {charity_total_element.text}")
+                    
+                    # Formatting
+                    total_text = charity_total_element.text[:-2].replace(' ', '')
                     current_value = int(total_text)
+
                     if current_value > previous_value:
                         donation = current_value - previous_value
-                        print(
-                            f"-----> SOMEONE DONATED {donation} kr!")
+                        logging.info(f"Donation detected: {donation} kr")
+
+                        if (current_value - previous_value) >= 200:
+                            payload = {"event": "sprint_donation",
+                                       "message": f"🎉 SPRINT DONATION! {donation} kr 🎉"}
+                            await ws_connection.send(str(payload))
+                            _ = await ws_connection.recv() # Hold for response
+                            logging.info("Turbo donation event sent")
+
+                        else:
+                            payload = {"event": "donation",
+                                       "message": f"En hjälte skänkte {donation} kr"}
+                            await ws_connection.send(str(payload))
+                            _ = await ws_connection.recv() # Hold for response
+                            logging.info("Donation event sent")
+                        
                         previous_value = current_value
                         with open("current_value.txt", "w", encoding="utf-8") as f:
                             f.write(f"{current_value} kr")
                             f.close()
-
-                        if (current_value - previous_value >= 200):
-                            payload = {
-                                "message": f"🎉 TUBRO DONATION! {donation} kr 🎉"}
-                            print(payload)
-                            await ws_connection.send(str(payload))
-                            reply = await ws_connection.recv()
-                            print(f"Received: {reply}")
-                        else:
-                            payload = {
-                                "message": f"En hjälte skänte {donation} kr"}
-                            await ws_connection.send(str(payload))
-                            reply = await ws_connection.recv()
-                            print(f"Received: {reply}")
                 else:
-                    print("Charity total not found")
+                    logging.error("Charity total not found")
 
             except Exception as e:
-                print("Error:", e)
+                logging.error("Unexpected exception while scraping:", e)
 
-            time.sleep(10)
+            time.sleep(REFRESH_RATE)
 
     except KeyboardInterrupt:
-        print("\nClearing up resources and exiting gracefully...")
+        logging.info("Clearing up resources and exiting gracefully...")
         await ws_connection.close()
         driver.quit()
         service.stop()
